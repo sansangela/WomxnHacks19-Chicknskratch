c7d41799aabb9cb3ac8c3f2585da7a595a92bd74
Yichen Li <yichenli@ucsb.edu>
2019-01-26 20:46:04 -0800

    corrected typo

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var img;
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	frameRate(20);
-	//img = loadImage('png/chicken1betterlineweight.png');
+	img = loadImage('png/chicken1betterlineweight.png');
 }
 
 function draw() {
