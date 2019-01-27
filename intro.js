7e02b659d6204fc14de17ac3f0861946eaf209a6
Yichen Li <yichenli@ucsb.edu>
2019-01-26 23:51:35 -0800

    Update intro.js

--- intro.js
+++ intro.js
@@ -2,13 +2,11 @@ var scribble = new Scribble();
 var windowHeight = window.innerHeight*0.6;
 var windowWidth = window.innerWidth*2/3;
 
-let points;
-let bounds;
 let font;
 var myCanvas;
 
-function preload() {
-  font = loadFont('avenir.otf');
+function preload(){
+	font = loadFont('avenir.otf');
 }
 
 function setup() {
@@ -16,20 +14,13 @@ function setup() {
 	myCanvas.parent("shake");
 	stroke(0);
 	fill(0);
-	textSize(40);
-	points = font.textToPoints("move left hand to change amplitude, right hand for pitch", 0, 0, 10, {
-		sampleFactor: 5,
-		simplifyThreshold: 0
-	});
 }
 
 function draw() {
 	background(255);
 	stroke(0);
-	let margin = 10;
-	for (var i = 0; i < points.length; i++) {
-		var pt = points[i];
-		scribble.scribbleEllipse( pt.x, pt.y, 1, 1 );
-	}
+	textFont(font);
+	textSize(30);
+	text("move left hand to change amplitude, right hand for pitch",windowWidth/10, windowHeight/2);
 }
 
