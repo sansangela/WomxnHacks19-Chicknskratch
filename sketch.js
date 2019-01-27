1fff952c9d1dc03714d48f1703e5b47bc6fce82a
Yichen Li <yichenli@ucsb.edu>
2019-01-26 21:26:36 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -29,7 +29,7 @@ function draw() {
 	scribble.scribbleLine(originX, originY, endX, originY );
 	scribble.scribbleLine(originX, originY-windowHeight/2, originX, originY);
 	scribble.scribbleRect(originX,originY,15,15);
-	image(img, mouseX-img.width/(2*15)+displacementChickenX, originY-img.height/(15*2)+displacementChickenY, img.width/15, img.height/15);
+	image(img, mouseX-img.width/(2*10)+displacementChickenX, originY-img.height/(10*2)+displacementChickenY, img.width/10, img.height/10);
 	for (let i=1; i<8; i+=4){
 		scribble.scribbleCurve(originX+(lenOfCurve*(i-1)), originY, originX+lenOfCurve*i, originY-(2*lenOfCurve/pi), originX+(lenOfCurve*(i-1))+(0.3642*lenOfCurve), originY, originX+(lenOfCurve*(i-1))+lenOfCurve*0.6358, originY-(2*lenOfCurve/pi));
 		scribble.scribbleCurve(originX+lenOfCurve*i, originY-(2*lenOfCurve/pi), originX+(lenOfCurve*(i+1)), originY, originX+lenOfCurve*i+(0.3642*lenOfCurve),originY-(2*lenOfCurve/pi),originX+(lenOfCurve*(i+1))-(0.3642*lenOfCurve),originY);
