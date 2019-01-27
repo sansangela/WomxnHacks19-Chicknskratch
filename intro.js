e03338c3e9d574f3bce5d9f1c1d6c8c956e9c965
Yichen Li <yichenli@ucsb.edu>
2019-01-26 23:27:03 -0800

    Update intro.js

--- intro.js
+++ intro.js
@@ -16,7 +16,7 @@ function setup() {
 	myCanvas.parent("shake");
 	stroke(0);
 	fill(0);
-	points = font.textToPoints('"move left hand to change amplitude, right hand for pitch"', 0, 0, 10, {
+	points = font.textToPoints("move left hand to change amplitude, right hand for pitch", 0, 0, 10, {
 		sampleFactor: 5,
 		simplifyThreshold: 0
 	});
@@ -26,8 +26,9 @@ function draw() {
 	background(255);
 	stroke(0);
 	let margin = 10;
-	for (var i = 0; i < points.length; i++) {
+	text("move left hand to change amplitude, right hand for pitch")
+/*	for (var i = 0; i < points.length; i++) {
 		var pt = points[i];
-		scribble.scribbleEllipse( pt.x, pt.y, 5, 5 );
+		scribble.scribbleEllipse( pt.x, pt.y, 5, 5 );*/
   }
 }
