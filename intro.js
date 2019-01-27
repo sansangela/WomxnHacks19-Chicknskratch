c72814403bc523560887e1672cf5dbb00a6f1a40
Yichen Li <yichenli@ucsb.edu>
2019-01-26 23:16:54 -0800

    Update intro.js

--- intro.js
+++ intro.js
@@ -19,22 +19,14 @@ function setup() {
     sampleFactor: 5,
     simplifyThreshold: 0
     });
-    bounds = font.textBounds('"move left hand to change amplitude, right hand for pitch"', 0, 0, 10);
 }
 
 function draw() {
 	background(255);
 	stroke(0);
 	let margin = 10;
-	beginShape();
-	translate(-bounds.x * width / bounds.w, -bounds.y * height / bounds.h);
-	for (let i = 0; i < points.length; i++) {
-		let p = points[i];
-		vertex(
-			p.x * width / bounds.w +
-			sin(20 * p.y / bounds.h + millis() / 1000) * width / 30,
-			p.y * height / bounds.h
-			);
-	}
-	endShape(CLOSE);
+	for (var i = 0; i < points.length; i++) {
+		var pt = points[i];
+		scribble.scribbleEllipse( pt.x, pt.y, 5, 5 );
+  }
 }
